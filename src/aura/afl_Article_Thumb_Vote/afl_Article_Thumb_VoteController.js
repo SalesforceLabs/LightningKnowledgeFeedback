19b5c48a72eadbe3534029a33046616995c791bd
Eniley Albuerne <ealbuerne@altimetrik.com>
2018-11-23 11:44:16 -0300

    Resolved #848 - [BUG] Error is displayed when text inserted on the 'Feedback vote description' field exceeds the 30000 limit Resolved #844 - Register votes in the Knowledge Article

--- src/aura/afl_Article_Thumb_Vote/afl_Article_Thumb_VoteController.js
+++ src/aura/afl_Article_Thumb_Vote/afl_Article_Thumb_VoteController.js
@@ -1,16 +1,16 @@
 ({
 	doInit : function(component, event, helper) {
-		//Initialize picklist data
-		helper.loadPicklistValues(component, event, helper);
+		//Initialize data
+		helper.getUserVote(component, event, helper);
 
 		var unlikeCardDivCmp = component.find("unlikeCardDiv");
-        $A.util.removeClass(unlikeCardDivCmp, "slds-show");
-        $A.util.addClass(unlikeCardDivCmp, "slds-hide");
+		$A.util.removeClass(unlikeCardDivCmp, "slds-show");
+		$A.util.addClass(unlikeCardDivCmp, "slds-hide");
+		component.set("v.validFeedbackDescription", true);
+		component.set("v.errorMessage", "");
 	},
 
 	handleToggleLike : function (component, event, helper) {
-
-        var dislike = component.get("v.disliked");
 
 		var unlikeCardDivCmp = component.find("unlikeCardDiv");
         $A.util.removeClass(unlikeCardDivCmp, "slds-show");
@@ -21,18 +21,20 @@
 
 		component.set("v.unlikeReason", "");
         component.set("v.undislikeescription","");
+		component.set("v.validFeedbackDescription", true);
+		component.set("v.errorMessage", "");
 		if(component.get("v.displayNegativeFeedbackDescription"))
         	helper.saveThumbVote(component, event);
     },
 
 	handleToggleDislike : function (component, event, helper) {
-
-        var dislike = component.get("v.disliked");
-
 		var unlikeCardDivCmp = component.find("unlikeCardDiv");
         $A.util.addClass(unlikeCardDivCmp, "slds-show");
         $A.util.removeClass(unlikeCardDivCmp, "slds-hide");
 		component.set("v.disliked", true);
+		component.set("v.liked", false);
+		component.set("v.validFeedbackDescription", true);
+		component.set("v.errorMessage", "");
     },
 
 	handleClick : function (component, event, helper) {
--- src/aura/afl_Article_Thumb_Vote/afl_Article_Thumb_VoteHelper.js
+++ src/aura/afl_Article_Thumb_Vote/afl_Article_Thumb_VoteHelper.js
@@ -13,7 +13,7 @@
        $A.util.addClass(spinner, "slds-hide");
    },
 
-   loadPicklistValues: function(component, event, helper) {
+   loadPicklistValues: function(component, ctx) {
 	   this.handleAction(component, null, 'c.getPickListValuesIntoList', this.loadPicklistValuesCallback);
    },
 
@@ -30,7 +30,47 @@
 	   }
    },
 
+   getUserVote: function(component, event, helper) {
+	   var actionParams = {
+		  'recordId': component.get("v.recordId")
+	   };
+	   this.handleAction(component, actionParams, 'c.getVote', this.getUserVoteCallback);
+   },
+
+   getUserVoteCallback: function(component, response, ctx) {
+	   if(!$A.util.isUndefinedOrNull(response)) {
+		 var result = JSON.parse(response.vote);
+
+		  if(result) {
+			  component.set("v.liked", true);
+			  component.set("v.disliked", false);
+			  component.set("v.savedVote",'5');
+		  } else {
+			  component.set("v.liked", false);
+			  component.set("v.disliked", true);
+			  component.set("v.savedVote",'1');
+
+			  //If article has a negative vote show reason's pickList
+			  if(component.get("v.savedVote") == "1") {
+				  var unlikeCardDivCmp = component.find("unlikeCardDiv");
+				  $A.util.removeClass(unlikeCardDivCmp, "slds-hide");
+				  $A.util.addClass(unlikeCardDivCmp, "slds-show");
+			  }
+		  }
+	  } else {
+		  //There's no vote so both buttons must be shown unchecked
+		  component.set("v.liked", false);
+		  component.set("v.disliked", false);
+	  }
+	  ctx.loadPicklistValues(component, ctx);
+   },
+
    saveThumbVote: function (component, event) {
+	   //Prevent user from voting the same again
+	   if((component.get("v.savedVote") == '5' && component.get('v.liked')) || (component.get("v.savedVote") == '1' && component.get("v.disliked")))
+		   return;
+	   if(!this.validateFeedbackDescription(component))
+	   	   return;
 	   this.showSpinner(component);
 	   var reason = component.get("v.unlikeReason");
 	   var description = component.get("v.voteReasonDescription");
@@ -48,7 +88,20 @@
    saveThumbVoteCallback: function (component, response, ctx) {
 	   component.set("v.unlikeReason","");
 	   component.set("v.voteReasonDescription","");
+	   component.set("v.savedVote", component.get("v.liked") ? '5' : '1');
+	   component.set("v.validFeedbackDescription", true);
+	   component.set("v.errorMessage", "");
 	   ctx.hideSpinner(component);
 	   ctx.showToast('SUCCESS', 'success', 'Feedback saved successfully', 'pester');
+   },
+
+   validateFeedbackDescription: function(component) {
+	   var value = component.get("v.voteReasonDescription");
+	   if(value.length > 29000){
+            component.set("v.validFeedbackDescription", false);
+			component.set("v.errorMessage", "Characters size length exceeded");
+			return false;
+        }
+		return true;
    }
 })
