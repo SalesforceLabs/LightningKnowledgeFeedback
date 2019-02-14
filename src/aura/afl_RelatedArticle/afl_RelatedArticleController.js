146bdc77cd16c88906341cc3f1ad4a6dfe98b3af
Mauricio Carrey <mcarrey@altimetrik.com>
2019-02-14 10:52:31 -0300

    Displays picklist on relatedArticle component when there are multiple languages for an article

--- src/aura/afl_RelatedArticle/afl_RelatedArticleController.js
+++ src/aura/afl_RelatedArticle/afl_RelatedArticleController.js
@@ -1,9 +1,19 @@
 ({
+	doInit: function(component, event, helper) {
+		helper.doInit(component, event, helper);
+	},
+
 	navigateTo : function(component, event, helper) {
 		helper.navigateTo(component, event, helper);
 	},
-	
-	doInit: function(cmp, event, helper) {
-		helper.doInit(cmp, event, helper);
-	}
+
+	handleLanguageChange: function (component, event, helper) {
+		var kavId = event.getParam("value");
+
+		if (component.get('v.originalKavId') === kavId) {
+			helper.doInit(component, event, helper, kavId);
+		} else {
+			helper.reloadArticle(component, event, helper, kavId);
+		}
+    }
 })
