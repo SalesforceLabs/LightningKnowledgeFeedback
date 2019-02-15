ea6689866b3362f18fb1c7d0b24b04edab82c321
Mauricio Carrey <mcarrey@altimetrik.com>
2019-02-15 12:14:48 -0300

    Resolved #915 - [BUG] [Article Feedback] Feedback posted in an English version of an article appears on the same article but in the Portuguese version For Feedbacks that originated from the trigger  the master language and a message explaining the scenario is explained.

--- src/aura/afl_RelatedArticle/afl_RelatedArticleController.js
+++ src/aura/afl_RelatedArticle/afl_RelatedArticleController.js
@@ -5,15 +5,5 @@
 
 	navigateTo : function(component, event, helper) {
 		helper.navigateTo(component, event, helper);
-	},
-
-	handleLanguageChange: function (component, event, helper) {
-		var kavId = event.getParam("value");
-
-		if (component.get('v.originalKavId') === kavId) {
-			helper.doInit(component, event, helper, kavId);
-		} else {
-			helper.reloadArticle(component, event, helper, kavId);
-		}
-    }
+	}
 })
--- src/aura/afl_RelatedArticle/afl_RelatedArticleHelper.js
+++ src/aura/afl_RelatedArticle/afl_RelatedArticleHelper.js
@@ -8,14 +8,11 @@
 		if (!component.isValid()) return;
 
 		component.set("v.kavId", response.kavId);
-		component.set("v.originalKavId", response.kavId);
 		component.set('v.articleLanguage', response.articleLanguage);
 		component.set('v.articleRecordType', response.articleRecordType);
 		component.set('v.articleNumber', response.articleNumber);
 		component.set('v.articlePublishingStatus', response.articlePublishingStatus);
-		component.set('v.originalArticleVersion', response.articleVersion);
 		component.set('v.originatedFromTrigger', response.originatedFromTrigger);
-		component.set('v.hasMultipleLanguages', response.hasMultipleLanguages);
 
 		if (response.isArchived) {
 			component.set('v.articleVersion', response.articleVersion + ' (deprecated)');
@@ -27,18 +24,6 @@
 			toastCmp.set("v.severity", 'warning');
 		} else {
 			component.set('v.articleVersion', response.articleVersion);
-		}
-
-		if (response.hasMultipleLanguages) {
-			var languages = [];
-			var languagesMap = response.languagesMap;
-			for (var key in languagesMap) {
-                languages.push({value:key, label:languagesMap[key]});
-				console.log('key: ' + key);
-				console.log('languagesMap[key]: ' + languagesMap[key]);
-            }
-
-			component.set("v.languagesMap", languages);
 		}
 	},
 
@@ -50,33 +35,5 @@
 			"slideDevName": "detail"
 		});
 		sObectEvent.fire();
-	},
-
-	reloadArticle : function(component, event, helper, kavId) {
-		var actionParams = {kavId: kavId};
-		this.handleAction(component, actionParams, 'c.reloadKav', this.reloadArticleCallback);
-	},
-
-	reloadArticleCallback : function(component, response, ctx) {
-		if (!component.isValid()) return;
-
-		component.set("v.kavId", response.kavId);
-		component.set('v.articleVersion', response.articleVersion);
-		component.set('v.articleLanguage', response.articleLanguage);
-		component.set('v.articleRecordType', response.articleRecordType);
-		component.set('v.articleNumber', response.articleNumber);
-		component.set('v.articlePublishingStatus', response.articlePublishingStatus);
-
-		if (response.isArchived) {
-			component.set('v.articleVersion', response.articleVersion + ' (deprecated)');
-
-			var toastCmp = component.find("toastNotif");
-			toastCmp.set("v.title", 'WARNING');
-			toastCmp.set("v.description", 'The related Knowledge Article is archived.');
-			toastCmp.set("v.className", '');
-			toastCmp.set("v.severity", 'warning');
-		} else {
-			component.set('v.articleVersion', response.articleVersion);
-		}
 	}
 })
