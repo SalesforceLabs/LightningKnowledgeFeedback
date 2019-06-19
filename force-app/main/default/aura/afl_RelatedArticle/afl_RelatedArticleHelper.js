7c30e3b2c917e5aa0cf3ee957246219c5fd1c7a8
Mauricio Carrey <mcarrey@altimetrik.com>
2019-06-19 09:59:16 -0300

    Initial commit with sdfx structure

---
+++ force-app/main/default/aura/afl_RelatedArticle/afl_RelatedArticleHelper.js
@@ -0,0 +1,39 @@
+({
+	doInit : function (component, event, helper) {
+		var actionParams = {recordId: component.get("v.recordId")};
+		this.handleAction(component, actionParams, 'c.loadKAVId', this.doInitCallback);
+	},
+
+	doInitCallback : function(component, response, ctx) {
+		if (!component.isValid()) return;
+
+		component.set("v.kavId", response.kavId);
+		component.set('v.articleLanguage', response.articleLanguage);
+		component.set('v.articleRecordType', response.articleRecordType);
+		component.set('v.articleNumber', response.articleNumber);
+		component.set('v.articlePublishingStatus', response.articlePublishingStatus);
+		component.set('v.originatedFromTrigger', response.originatedFromTrigger);
+
+		if (response.isArchived) {
+			component.set('v.articleVersion', response.articleVersion + ' (deprecated)');
+
+			var toastCmp = component.find("toastNotif");
+			toastCmp.set("v.title", 'WARNING');
+			toastCmp.set("v.description", 'The related Knowledge Article is archived.');
+			toastCmp.set("v.className", '');
+			toastCmp.set("v.severity", 'warning');
+		} else {
+			component.set('v.articleVersion', response.articleVersion);
+		}
+	},
+
+	navigateTo : function(component, event, helper) {
+		var idKav = component.get("v.kavId");
+		var sObectEvent = $A.get("e.force:navigateToSObject");
+		sObectEvent .setParams({
+			"recordId": idKav,
+			"slideDevName": "detail"
+		});
+		sObectEvent.fire();
+	}
+})
--- src/aura/afl_ArticleFeedbackConfigContainer/afl_ArticleFeedbackConfigContainer.app
+++
@@ -1,3 +0,0 @@
-<aura:application access="GLOBAL" extends="ltng:outApp">
-    <c:afl_ArticleFeedbackConfiguration/>
-</aura:application>
