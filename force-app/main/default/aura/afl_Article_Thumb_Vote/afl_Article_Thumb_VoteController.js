e06f35b8e5fd22197cc44f31f03118056cf2f9f8
Mauricio Carrey <mcarrey@altimetrik.com>
2019-09-20 13:00:04 -0300

    Resolved #1149 - [Task] [Article Feedback] Always display Feedback Reason picklist (positive and negative vote)

--- force-app/main/default/aura/afl_Article_Thumb_Vote/afl_Article_Thumb_VoteController.js
+++ force-app/main/default/aura/afl_Article_Thumb_Vote/afl_Article_Thumb_VoteController.js
@@ -23,14 +23,8 @@
 		component.set("v.liked", true);
 
 		var feedbackDivContainerCmp = component.find("feedbackDivContainer");
-		$A.util.removeClass(feedbackDivContainerCmp, "slds-hide");
 		$A.util.addClass(feedbackDivContainerCmp, "slds-show");
-
-		if (!component.get("v.alwaysDisplayFeedDescription")) {
-			$A.util.removeClass(feedbackDivContainerCmp, "slds-show");
-			$A.util.addClass(feedbackDivContainerCmp, "slds-hide");
-			helper.saveThumbVote(component, event);
-		}
+		$A.util.removeClass(feedbackDivContainerCmp, "slds-hide");
 	},
 
 	handleToggleDislike : function (component, event, helper) {
