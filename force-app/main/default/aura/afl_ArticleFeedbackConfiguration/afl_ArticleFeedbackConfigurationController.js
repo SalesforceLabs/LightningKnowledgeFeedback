3be10d1e228fe322f875dcf51368ee4e7143ae4b
Mauricio Carrey <mcarrey@altimetrik.com>
2020-02-20 10:19:36 -0300

    #1216 - [Task] Remove reason picklist management from Setup and code Removes mentios to picklist values from setup component, classes and test

--- force-app/main/default/aura/afl_ArticleFeedbackConfiguration/afl_ArticleFeedbackConfigurationController.js
+++ force-app/main/default/aura/afl_ArticleFeedbackConfiguration/afl_ArticleFeedbackConfigurationController.js
@@ -5,25 +5,5 @@
 
 	updateHashtag: function(component, event, helper) {
 		helper.updateHashtagHelper(component);
-	},
-
-	addPicklistValue: function(component, event, helper) {
-		helper.addPicklistValueHelper(component);
-	},
-
-	handlePositiveChange: function(component, event, helper) {
-		helper.saveValueOrderHelper(component, event);
-	},
-
-	handleNegativeChange: function(component, event, helper) {
-		helper.saveValueOrderHelper(component, event);
-	},
-
-	saveValueOrder: function(component, event, helper) {
-		helper.saveValueOrderHelper(component, event);
-	},
-
-	deleteValue: function(component, event, helper) {
-		helper.deleteValueHelper(component, event);
 	}
 })
