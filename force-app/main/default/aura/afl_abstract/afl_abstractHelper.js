773f3bb7bef5ce19888eac99b36558721f92e400
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-05-07 09:37:34 -0300

    Resolved #1271 Remove the modal that reads "The related article could not be found

--- force-app/main/default/aura/afl_abstract/afl_abstractHelper.js
+++ force-app/main/default/aura/afl_abstract/afl_abstractHelper.js
@@ -13,38 +13,44 @@
 				}
 
 				var result = a.getReturnValue();
-
 				// Some error likely inside of the Apex code occurred.
 				if (result.state !== 'SUCCESS') {
 					// Try to get the error message from the lightningdmlerror object
-
-					var errorEncountered;
-					if (!$A.util.isUndefinedOrNull(result.errors)) {
-						errorEncountered = result.errors[0].message;
-					} else {
-						if (!$A.util.isUndefinedOrNull(result.error)) {
-							errorEncountered = result.error;
+					if(result.state != 'ERRORNOTFOUND'){
+						var errorEncountered;
+						if (!$A.util.isUndefinedOrNull(result.errors)) {
+							errorEncountered = result.errors[0].message;
+						} else {
+							if (!$A.util.isUndefinedOrNull(result.error)) {
+								errorEncountered = result.error;
+							}
 						}
+						throw {
+							'message' : 'An error occurred in the apex call',
+							'extendedMessage' : errorEncountered
+						};
 					}
-
-					throw {
-						'message' : 'An error occurred in the apex call',
-						'extendedMessage' : errorEncountered
-					};
 				}
 
 				var returnValue = undefined;
+				if (result.state == 'ERRORNOTFOUND') {
+					// remove the modal that reads "The related article could not be found
+					Response.error();
+				}
 				if (!$A.util.isEmpty(result.jsonResponse)) {
 					// Will throw a JSON exception if the result cannot be parsed.
 					returnValue = JSON.parse(result.jsonResponse);
 				}
-
 				var concreteComponent = component.getConcreteComponent();
 				successCallback(concreteComponent,returnValue, self);
 			} catch(ex) {
 				// Handle any exceptions encountered in the callback
 				var errorTitle = "An error occurred";
 				var errorMessage = ex.message;
+				if(ex.message=='Response.error is not a function'){
+					//just show that can found the article and nothing else
+				}
+				else{
 
 				// Add a detailed description of the error if one is found.
 				if (!$A.util.isEmpty(ex.extendedMessage)) {
@@ -57,6 +63,7 @@
 					errorCallback(component, errorTitle, errorMessage);
 				}
 			}
+			}
 		});
 
 		$A.enqueueAction(action);
@@ -67,8 +74,8 @@
 	},
 
 	showToast : function(type, title, message, mode) {
+		
 		var toastEvent = $A.get("e.force:showToast");
-
 		if (!$A.util.isUndefinedOrNull(toastEvent)) {
 			toastEvent.setParams({
 				"title" : title,
